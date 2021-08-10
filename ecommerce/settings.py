49afeebf219bb8dd052c19efec88ee0c725736e1
vizz11 <62127081+vizz11@users.noreply.github.com>
2021-08-10 13:52:53 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -137,10 +137,12 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'logout'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
-
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 CART_SESSION_ID = 'cart'
