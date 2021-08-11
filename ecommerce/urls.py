9088f4506ec006f013da24aaf1701059dd4466ac
vizz11 <62127081+vizz11@users.noreply.github.com>
2021-08-11 17:34:58 +0530

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -38,8 +38,8 @@ urlpatterns = [
     path('orders/', include('orders.urls',namespace='orders')),
     path('', include('shop.urls',namespace='shop')),
 ]
-urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
